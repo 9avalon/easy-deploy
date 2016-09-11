1f0d9c751a0f69cc5eeb08bda67e6dc62fa82368
unknown <544461655@qq.com>
2016-09-11 17:26:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# easydeploy
+# easy-deploy
 自动化重启tomcat的脚本，手工替换掉war包后，运行此脚本以重启tomcat
+
+
+
+### Introduction
+
+重启tomcat需要执行一系列的步骤，包括执行shutdown脚本，清理缓存，再执行startup脚本。有时候因为进程的原因，shutdown脚本并不能完全将tomcat关闭，还需要使用ps、kill等命令将tomcat进程杀死，如果经常要部署重启项目，绝对是十分繁琐和痛苦的。所以考虑将这一系列步骤用脚本自动化。
+
+
+
+脚本的执行步骤为：
+
+1.获取该tomcat的pid
+
+2.检测pid是否存在，存在则进行shutdown和kill操作
+
+3.清理tomcat目录下的缓存
+
+4.重启tomcat
+
+5.控制台打印tomcat启动日志
+
+
+
+### Usage
+
+1.将easydeploy脚本放到tomcat的bin目录下
+
+2.编辑配置文件
+
+```sh
+# DEFINE
+tomcat_name="xxxxx";
+war_name="xxxxx"
+```
+
+tomcat_name为tomcat文件夹的名字
+
+war_name为部署的项目war包名字
+
+3.执行脚本即可
+
---
+++ easy-deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# author : mingjian_hou
+# desc : shut down the tomcat, clear the cache and startup it, please put this script in /tomcat/bin/
+
+# DEFINE
+tomcat_name="tomcat";
+war_name="xxxxx"
+
+# get tomcat pid and judge should shutdown or not
+tomcat_id=$(ps -aux | grep -w .*"$tomcat_name".*Bootstrap.* | grep -v 'grep' | awk '{print $2}')
+
+if [ ! -n "$tomcat_id" ]; then
+    echo "===== not found tomcat's pid, tomcat already close complete ====="
+else 
+    # run shut down 
+    shutdown.sh
+    echo "===== closing the tomcat, waiting ... ====="
+    sleep 3
+    
+    # recheck the tomcat pid exist or not
+    tomcat_id=$(ps -aux | grep -w .*"$tomcat_name".*Bootstrap.* | grep -v 'grep' | awk '{print $2}')
+    if [ -n "$tomcat_id" ]; then 
+        echo "===== found tomcat's pid:"$tomcat_id", kill it.. ======"
+        kill -9 $tomcat_id
+    fi
+fi
+
+# remove the tomcat cache
+rm -rf ../webapps/$war_name
+rm -rf ../work/Catalina/*
+rm -rf ../temp/*
+# restart tomcat
+startup.sh
+
+# tail the log
+tail -f ../logs/catalina.out
